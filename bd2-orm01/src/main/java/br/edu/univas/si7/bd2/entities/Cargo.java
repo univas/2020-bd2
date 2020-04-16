c63d4b9cefb264d072b4d35eb47aa2f47eea0610
Roberto Rocha <rrocha.roberto@gmail.com>
2020-04-16 15:18:43 -0300

    First Hibernate project.

---
+++ bd2-orm01/src/main/java/br/edu/univas/si7/bd2/entities/Cargo.java
@@ -0,0 +1,74 @@
+package br.edu.univas.si7.bd2.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Cargo implements Serializable { // nosso POJO
+
+	@Id
+	private int codigo;
+	private String nome;
+
+	public Cargo() {
+
+	}
+
+	public Cargo(int codigo, String nome) {
+		super();
+		this.codigo = codigo;
+		this.nome = nome;
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public String toString() {
+		return "Cargo [codigo=" + codigo + ", nome=" + nome + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + codigo;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cargo other = (Cargo) obj;
+		if (codigo != other.codigo)
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
+
+}
